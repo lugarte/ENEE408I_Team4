0133aa3228ed02c43e22766bf4112af92cb6b1b5
lugarte <60938067+lugarte@users.noreply.github.com>
2020-05-03 13:14:45 -0400

    working test version

--- alexa:jetson_coms/FlaskAskDecideTask.py
+++ alexa:jetson_coms/FlaskAskDecideTask.py
@@ -1,4 +1,5 @@
 # Listens to commands from Alexa, sends command to robot over serial
+# Test version of code that sends serial command 'n' over serial to arduino, prompting robot to navigate around the room
 
 import serial
 
@@ -8,36 +9,43 @@ from flask_ask import Ask, statement
 app = Flask(__name__)
 ask = Ask(app, '/')
 
-@ask.intent('EnforceQuarantine')
-def enforce_quarantine():
-	speech_text = 'Enforcing Quarantine'
-	ser.write(b'e')
-	return statement(speech_text).simple_card('Mary', speech_text)
+@ask.intent('MoveIntent')
+def navigate():
+	speech_text = 'Navigating...'
+	ser.write(b'n')
+	return statement(speech_text).simple_card('robot tester', speech_text)
 
-@ask.intent('HandWashReminder')
-def hand_wash_reminder():
-	speech_text = 'Setting handwashing reminder...'
-	ser.write(b'h')
-	return statement(speech_text).simple_card('Mary', speech_text)
 
-@ask.intent('DetectFall')
-def detect_fall():
-	speech_text = 'Looking out for falls...'
-	ser.write(b'd')
-	return statement(speech_text).simple_card('Mary', speech_text)
+# @ask.intent('EnforceQuarantine')
+# def enforce_quarantine():
+#	speech_text = 'Enforcing Quarantine'
+#	ser.write(b'e')
+#	return statement(speech_text).simple_card('Mary', speech_text)
 
-@ask.intent('OrderGroceries')
-def order_groceries():
-	speech_text = 'Ordering groceries...'
-	ser.write(b'o')
-	return statement(speech_text).simple_card('Mary', speech_text)
+#@ask.intent('HandWashReminder')
+#def hand_wash_reminder():
+#	speech_text = 'Setting handwashing reminder...'
+#	ser.write(b'h')
+#	return statement(speech_text).simple_card('Mary', speech_text)
 
-@ask.intent('EnforceSocialDistancing')
-def enforce_quarantine():
-	speech_text = 'Enforcing social distancing...'
-	ser.write(b's')
-	return statement(speech_text).simple_card('Mary', speech_text)
+#@ask.intent('DetectFall')
+#def detect_fall():
+#	speech_text = 'Looking out for falls...'
+#	ser.write(b'd')
+#	return statement(speech_text).simple_card('Mary', speech_text)
+
+#@ask.intent('OrderGroceries')
+#def order_groceries():
+#	speech_text = 'Ordering groceries...'
+#	ser.write(b'o')
+#	return statement(speech_text).simple_card('Mary', speech_text)
+
+#@ask.intent('EnforceSocialDistancing')
+#def enforce_quarantine():
+#	speech_text = 'Enforcing social distancing...'
+#	ser.write(b's')
+#	return statement(speech_text).simple_card('Mary', speech_text)
 
 if __name__ == '__main__':
-	ser = serial.Serial('/dev/cu.usbmodem1411', 9600)
+	ser = serial.Serial('/dev/ttyACM1', 9600)
 	app.run()
